f1a3733c87f7ec7ba129dccd8c425c133393636a
Sakib Hasan <sakib100.sa@gmail.com>
2022-05-06 04:17:03 +0600

    portfolio area completed

--- src/Components/Sections/Portfolio/Portfolio.js
+++ src/Components/Sections/Portfolio/Portfolio.js
@@ -1,7 +1,114 @@
 import React from "react";
 import "./Portfolio.css";
+import image1 from "../../../Resources/img/portfolio/portfolio-1.jpg";
+import image2 from "../../../Resources/img/portfolio/portfolio-2.jpg";
+import image3 from "../../../Resources/img/portfolio/portfolio-3.jpg";
+import image4 from "../../../Resources/img/portfolio/portfolio-4.jpg";
+import image5 from "../../../Resources/img/portfolio/portfolio-5.jpg";
+import image6 from "../../../Resources/img/portfolio/portfolio-6.jpg";
+import image7 from "../../../Resources/img/portfolio/portfolio-7.jpg";
+import image8 from "../../../Resources/img/portfolio/portfolio-8.jpg";
+import image9 from "../../../Resources/img/portfolio/portfolio-9.jpg";
+import { AiOutlinePlus } from "react-icons/ai";
+import { BiLink } from "react-icons/bi";
 
 const Portfolio = () => {
+	const portfolioItems = [
+		{
+			_id: 1,
+			name: "Web Design 1",
+			Category: "Web Design",
+			Client: "ASU Company",
+			ProjectDate: "01 March, 2020",
+			ProjectURL: "www.example.com",
+			projectImg: image1,
+		},
+		{
+			_id: 2,
+			name: "Graphics Design 1",
+			Category: "Card Design",
+			Client: "ASU Company",
+			ProjectDate: "01 March, 2020",
+			ProjectURL: "www.example.com",
+			projectImg: image2,
+		},
+		{
+			_id: 3,
+			name: "Web Design 2",
+			Category: "Web Design",
+			Client: "ASUS Company",
+			ProjectDate: "01 March, 2020",
+			ProjectURL: "www.example.com",
+			projectImg: image3,
+		},
+		{
+			_id: 4,
+			name: "Graphics Design 2",
+			Category: "Card Design",
+			Client: "ASUS Company",
+			ProjectDate: "01 March, 2020",
+			ProjectURL: "www.example.com",
+			projectImg: image4,
+		},
+		{
+			_id: 5,
+			name: "App Design 1",
+			Category: "App Design",
+			Client: "ASUS Company",
+			ProjectDate: "01 March, 2020",
+			ProjectURL: "www.example.com",
+			projectImg: image5,
+		},
+		{
+			_id: 6,
+			name: "App Design 2",
+			Category: "App Design",
+			Client: "ASUS Company",
+			ProjectDate: "01 March, 2020",
+			ProjectURL: "www.example.com",
+			projectImg: image6,
+		},
+		{
+			_id: 7,
+			name: "Web Design 3",
+			Category: "Web Design",
+			Client: "ASUS Company",
+			ProjectDate: "01 March, 2020",
+			ProjectURL: "www.example.com",
+			projectImg: image7,
+		},
+		{
+			_id: 8,
+			name: "Card Design 3",
+			Category: "Card Design",
+			Client: "ASUS Company",
+			ProjectDate: "01 March, 2020",
+			ProjectURL: "www.example.com",
+			projectImg: image8,
+		},
+		{
+			_id: 9,
+			name: "Web Design 4",
+			Category: "Web Design",
+			Client: "ASUS Company",
+			ProjectDate: "01 March, 2020",
+			ProjectURL: "www.example.com",
+			projectImg: image9,
+		},
+	];
+
+	const appDesigns = portfolioItems.filter(
+		(portfolioItem) => portfolioItem.Category === "App Design"
+	);
+
+	const cardDesigns = portfolioItems.filter(
+		(portfolioItem) => portfolioItem.Category === "Card Design"
+	);
+
+	const webDesigns = portfolioItems.filter(
+		(portfolioItem) => portfolioItem.Category === "Web Design"
+	);
+
 	return (
 		<section className="portfolio">
 			<div className="content p-3">
@@ -84,7 +191,27 @@ const Portfolio = () => {
 							role="tabpanel"
 							aria-labelledby="pills-all-tab"
 						>
-							All
+							<div className="cardsArea row">
+								{portfolioItems.map((portfolioItem) => (
+									<div className="cardArea col-12 col-sm-12 col-md-4 col-lg-4 col-xl-4 col-xxl-4 mb-4">
+										<div className="cardContent">
+											<img
+												src={portfolioItem.projectImg}
+												alt=""
+												className="img-fluid"
+											/>
+											<div className="links d-flex align-items-center">
+												<div className="w-50 text-center bg border-end">
+													<AiOutlinePlus className="icon" />
+												</div>
+												<div className="w-50 text-center bg ">
+													<BiLink className="icon" />
+												</div>
+											</div>
+										</div>
+									</div>
+								))}
+							</div>
 						</div>
 						<div
 							class="tab-pane fade"
@@ -92,7 +219,20 @@ const Portfolio = () => {
 							role="tabpanel"
 							aria-labelledby="pills-app-tab"
 						>
-							App
+							<div className="cardsArea row">
+								{appDesigns.map((appDesign) => (
+									<div className="cardArea col-12 col-sm-12 col-md-4 col-lg-4 col-xl-4 col-xxl-4 mb-4">
+										<div className="cardContent">
+											<img
+												src={appDesign.projectImg}
+												alt=""
+												className="img-fluid"
+											/>
+											<div className="links"></div>
+										</div>
+									</div>
+								))}
+							</div>
 						</div>
 						<div
 							class="tab-pane fade"
@@ -100,7 +240,20 @@ const Portfolio = () => {
 							role="tabpanel"
 							aria-labelledby="pills-card-tab"
 						>
-							Card
+							<div className="cardsArea row">
+								{cardDesigns.map((cardDesign) => (
+									<div className="cardArea col-12 col-sm-12 col-md-4 col-lg-4 col-xl-4 col-xxl-4 mb-4">
+										<div className="cardContent">
+											<img
+												src={cardDesign.projectImg}
+												alt=""
+												className="img-fluid"
+											/>
+											<div className="links"></div>
+										</div>
+									</div>
+								))}
+							</div>
 						</div>
 						<div
 							class="tab-pane fade"
@@ -108,7 +261,20 @@ const Portfolio = () => {
 							role="tabpanel"
 							aria-labelledby="pills-web-tab"
 						>
-							Web
+							<div className="cardsArea row">
+								{webDesigns.map((webDesign) => (
+									<div className="cardArea col-12 col-sm-12 col-md-4 col-lg-4 col-xl-4 col-xxl-4 mb-4">
+										<div className="cardContent">
+											<img
+												src={webDesign.projectImg}
+												alt=""
+												className="img-fluid"
+											/>
+											<div className="links"></div>
+										</div>
+									</div>
+								))}
+							</div>
 						</div>
 					</div>
 				</div>
